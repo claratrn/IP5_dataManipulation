9d1f86f85719256d0a69e54e312927f00c95aa82
Clara Tran <clara.tran@students.fhnw.ch>
2024-06-14 16:46:12 +0200

    plot

--- tools/Boxplots.py
+++ tools/Boxplots.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import os
 
 # Load the CSV files
-# gsm8k_df = pd.read_csv('../result_metrics/openAi/commonsense_qa/all_metrics_GPT_commonsense.csv')
+# commonsense_df = pd.read_csv('../result_metrics/openAi/commonsense_qa/all_metrics_GPT_commonsense.csv')
 # date_df = pd.read_csv('../result_metrics/openAi/dateUnd/all_metrics_date_GPT.csv')
-# commonsense_df = pd.read_csv('../result_metrics/openAi/gsm8k/allmetrics_gsm8k.csv')
+# gsm8k_df = pd.read_csv('../result_metrics/openAi/gsm8k/all_metrics_GPT_GSM8K.csv')
 # law_df = pd.read_csv('../result_metrics/openAi/mmlu/all_metrics_GPT_LAW.csv')
 
-gsm8k_df = pd.read_csv('../result_metrics/llama2/commonsense_qa/all_metrics_common_Llama2.csv')
+gsm8k_df = pd.read_csv('../result_metrics/llama2/gsm8k/all_metrics_gsm8k.csv')
 date_df = pd.read_csv('../result_metrics/llama2/bigbench/all_metrics_date_Llama2.csv')
-commonsense_df = pd.read_csv('../result_metrics/llama2/gsm8k/all_metrics_gsm8k_la.csv')
-law_df = pd.read_csv('../result_metrics/llama2/mmlu/all_metrics_law_llama.csv')
+commonsense_df = pd.read_csv('../result_metrics/llama2/commonsense_qa/all_metrics_common_Llama2.csv')
+law_df = pd.read_csv('../result_metrics/llama2/mmlu/all_metrics_llama2_mmlu.csv')
 # Add a column to each DataFrame to indicate the dataset
 gsm8k_df['dataset'] = 'GSM8K'
 date_df['dataset'] = 'Date'
@@ -35,7 +35,9 @@ def plot_metric_boxplots(metrics_df, output_dir, metric_name):
     for patch, color in zip(bplot['boxes'], colors):
         patch.set_facecolor(color)
 
-    plt.title(f'Comparison of {metric_name} - LlaMA 2')
+        plt.ylim(0, 1)
+
+    plt.title(f'Comparison of {metric_name} - Llama 2')
     plt.ylabel(metric_name)
     plt.xlabel('Method')
     plt.xticks(rotation=45)
